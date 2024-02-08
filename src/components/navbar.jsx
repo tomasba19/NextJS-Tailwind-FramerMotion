dca4ffbad00d4db4b3e36c657f8239b5197148c7
Tomas Barolo <tomasba19@gmail.com>
2024-02-08 14:29:41 -0300

    navBar Update

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { useState } from "react";
+import Image from "next/image";
+import NavLink from "./navLink";
 
 const links = [
   { url: "/", title: "Home" },
@@ -14,9 +16,15 @@ const NavBar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      {/* Links */}
+      <div className="hidden md:flex gap-4 w-1/3">
+        {links.map(link=>(
+          <NavLink link={link} key={link.title}/>
+        ))}
+      </div>
       {/* Logo */}
-      <div className="">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
@@ -27,16 +35,25 @@ const NavBar = () => {
           </span>
         </Link>
       </div>
+      {/* Social */}
+      <div className="hidden md:flex gap-4 w-1/3 justify-center">
+        <Link href="https://github.com/tomasba19">
+        <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/tom%C3%A1s-barolo-83918a276/">
+        <Image src="/linkedin.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
       {/* Navigation Menu */}
-      <div className="">
+      <div className="md:hidden">
         {/* Button*/}
-        <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
-          <div className="w-10 h-1 bg-white rounded"></div>
-          <div className="w-10 h-1 bg-white rounded"></div>
-          <div className="w-10 h-1 bg-white rounded"></div>
+        <button className="w-10 h-8 flex flex-col justify-between z-50 relative" onClick={(()=>setOpen(!open))}>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
         </button>
         {/* MENU*/}
-        {
+        {open && (
           <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
             {links.map((link) => (
               <Link href={link.url} key={link.title}>
@@ -45,7 +62,7 @@ const NavBar = () => {
               </Link>
             ))}
           </div>
-        }
+        )}
       </div>
     </div>
   );
