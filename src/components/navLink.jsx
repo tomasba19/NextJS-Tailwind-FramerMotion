5b94b26e582ed64f1addeeb1ec011f21a1c4df84
Tomas Barolo <tomasba19@gmail.com>
2024-02-12 00:46:18 -0300

    .

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -7,9 +7,7 @@ const NavLink = ({link}) => {
 const pathName =  usePathname()
 
     return (
-        <Link className={`rounded p-1 ${pathName=== link.url && "bg-black text-white"}`} href={link.url}>
-            {link.title}
-            </Link>
+        <Link className={`rounded p-1 ${pathName=== link.url && "bg-black text-white"}`} href={link.url}>{link.title}</Link>
     )
 }
 
