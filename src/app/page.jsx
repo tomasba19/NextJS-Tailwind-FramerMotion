cc500fdcb78ea394f16577cffb95a6292f179a84
Tomas Barolo <tomasba19@gmail.com>
2024-02-19 17:36:30 -0300

    fixes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,9 +11,9 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE COINTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <div className="h-1/2 lg:h-full lg:w-1/2 relative mr-12">
           <Image src="/homeImage.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT COINTAINER */}
@@ -27,11 +27,11 @@ const Homepage = () => {
             Make it work, make it right, make it fast.
           </p>
           {/* BUTTONS */}
-          <div className="w-full flex gap-4">
+          <div className="w-full flex gap-4 lg:w-1/2 flex-row items-center justify-center">
             <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
               Projects
             </button>
-            <button className="p-4 rounded-lg ring-1 ring-black ">
+            <button className="p-4 rounded-lg ring-1 ring-black">
               Contact Me
             </button>
           </div>
