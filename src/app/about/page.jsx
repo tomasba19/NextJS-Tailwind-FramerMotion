522068d499320c166b4c392ee54f1d98c41a2250
Tomas Barolo <tomasba19@gmail.com>
2024-02-18 22:50:09 -0300

    About-Page-Update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,18 +1,92 @@
 "use client";
 
-import {motion} from 'framer-motion';
-
+import { motion } from "framer-motion";
 
 const AboutPage = () => {
-    return (
-        <motion.div
+  return (
+    <motion.div
       className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
-    >About
+    >
+      {/*CONTAINER*/}
+      <div className="">
+        {/*TEXT CONTAINER*/}
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+          {/*BIOGRAPHY CONTAINER*/}
+          <div className="flex flex-col gap-12 justify-center">
+            {/*BIOGRAPHY TITTLE*/}
+            <h1 className="font-bold text-2xl">About Me</h1>
+            {/*BIOGRAPHY DESCRIPTION*/}
+            <p className="text-lg">
+              aca va otro text re contra re copado asodnaiosd asindion iasndin
+              iansd0in asidninasd i0ansdin asindoiasnd oinasn sns snns n nssd
+            </p>
+            {/*BIOGRAPHY QUOTE*/}
+            <span className="italic">asodn aoisnd ioasdn ioasnd iooin</span>
+            {/* BIOGRAPHY SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
+          {/*SKILLS CONTAINER*/}
+          <div className="flex flex-col gap-12 justify-center">
+            {/*SKILLS TITTLE*/}
+            <h1 className="font-bold text-2xl">Skills</h1>
+            {/*SKILLS LIST*/}
+            <div className=""></div>
+            {/*SKILLS SCROLL*/}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
+          </div>
+          {/*EXPERIENCE CONTAINER*/}
+          <div className="">EXPERIENCE</div>
+        </div>
+        {/*SVG CONTAINER*/}
+        <div className="hidden"></div>
+      </div>
     </motion.div>
-    )
-}
+  );
+};
 
 export default AboutPage;
